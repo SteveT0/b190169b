534c335361992b68662b454b126827ca4dc34b7e
SteveT0 <66001057+SteveT0@users.noreply.github.com>
2020-09-22 09:54:11 +0800

    Add files via upload

---
+++ B190169B.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FEIT</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<h1>Faculty of Engineering and Information Technology</h1>
+	<form class="reqform">
+		<h3>Request Form</h3>
+		<div id="box">Name : <input type="text" name=""></div>
+		<div id="box">Contact Number : <input type="text" name=""></div>
+		<div id="box">Year of Graduate : <input type="text" name=""></div>
+		<div id="box">Residential Area : <input type="text" name=""></div>
+		<button>Submit</button>
+	</form>
+	<div class="programme">
+		<h2>Programme Information</h2>
+		<p>Here lies all the information about the programme......</p>
+	</div>
+	<div class="link">
+		<a href="https://www.facebook.com/FEITSouthernUC"><img src="img/fb.png" width="50" height="50"></a>
+	</div>
+</body>
+</html>
